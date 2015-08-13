f76995ada61e4b92c276186d83e904feffb46117
albertomv83 <albertomv83@gmail.com>
2015-08-13 11:45:42 +0200

    Initial Commit

---
+++ src/es/instavino/wine/api/client/CatalogRestClient.java
@@ -0,0 +1,18 @@
+/**
+ * $Id$
+ * @author amancheno
+ * @date   13/8/2015 11:33:11
+ *
+ * Copyright (C) 2015 Scytl Secure Electronic Voting SA
+ *
+ * All rights reserved.
+ *
+ */
+package es.instavino.wine.api.client;
+
+/**
+ *
+ */
+public class CatalogRestClient {
+
+}
---
+++ src/es/instavino/wine/api/model/Status.java
@@ -0,0 +1,21 @@
+/**
+ * $Id$
+ * @author amancheno
+ * @date   13/8/2015 11:29:46
+ *
+ * Copyright (C) 2015 Scytl Secure Electronic Voting SA
+ *
+ * All rights reserved.
+ *
+ */
+package es.instavino.wine.api.model;
+
+/**
+ *
+ */
+public class Status {
+
+    private Integer ReturnCode;
+
+    private String[] Messages;
+}
